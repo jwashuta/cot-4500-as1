306398b0eb95c2c1fdf5a625888ef731cec5602f
Jonathan Washuta <122390678+jwashuta@users.noreply.github.com>
2023-01-26 12:33:19 -0500

    Update assignment_1.py

--- src/main/assignment_1.py
+++ src/main/assignment_1.py
@@ -3,16 +3,17 @@ import numpy as np
 import math
 
 # Question 1: Use double precision, calculate the resulting values (format to 5 decimal places)
-#       a) 0 | 10000000111 | 1110101110010000000000000000000000000000000000000000
+#       a) 0 | 10000000111 |pip 1110101110010000000000000000000000000000000000000000
 # formula to use: (-1)^s*2^c-1027*(1 + f)
 
 def double_precision():
-    # calculating the sign (s)
-    s = 0
-    print(s)
-    print("\n")
+    
+# calculating the sign (s)
 
-    # calculating the exponent (c)
+    s = 0
+
+# calculating the exponent (c)
+
     exponent = 10000000111
     c = 0
     i = 0
@@ -21,10 +22,9 @@ def double_precision():
         c = c + exp * pow(2, i)
         exponent = exponent // 10
         i += 1
-    print(c)
-    print("\n")
 
-    # calculating the fraction (f)
+# calculating the fraction (f)
+
     fraction = str(1110101110010000000000000000000000000000000000000000)
     f = 0
     i = 1
@@ -32,7 +32,7 @@ def double_precision():
         f = f + int(item) * ((1/2)**i)
         i += 1
     
-    # formula for converting binary to decimal (n)
+# formula for converting binary to decimal (n)
     
     n = ((-1)**s)*(2**(c - 1023))*(1 + f)
     print(f"{n:.5f}")
@@ -40,74 +40,99 @@ def double_precision():
 
 # Question 2: Repeat question 1 using three-digit chopping arithmetic
 
-n = n * (10**-3)
-math.floor((n*1000)/1000)
-print("\n")
+    new_n = n
+    new_n = n * (10**-3)
+    print((math.floor(new_n*1000))/1000)
+    print("\n")
 
 # Question 3: Repeat question 1 using three-digit rounding arithmetic
 
-n = n + 0.0005
-print(round(n, ndigits = 3))
-print("\n")
+    new_n = new_n + 0.0005
+    print(round(new_n, ndigits = 3))
+    print("\n")
 
 # Question 4: Compute the absolute and relative error with the exact value form question 1 and its 3 digit rounding
 
+    def absolute_error(x, xbar):
+        return abs(x-xbar)
+
+    def relative_error(x, xbar):
+        return 0 if x == 0 else absolute_error(x, xbar) / abs(x)
+    
+    def abs_rel_error():
+        number = 491.5625
+        rounded_number = 492
+        print(absolute_error(number, rounded_number))
+        print(relative_error(number, rounded_number))
+        return
+    abs_rel_error()
+    print("\n")
+
 # Question 5: What is the minimum number of terms needed to computer f(1) with error <10^-4?
-def question_5():
-    def series(x,k:int):
-        return ((-1)**k) * ((x**k)/(k**3))
+
+def error():
+    def series(x, k: int):
+        return ((-1)**k) * ((x**k) / (k**3))
     
     minError = 1e-4
     currentItteration = 1
-    while(abs(series(currentItteration)) > minError):
-        #print(serries(1,currentItteration))
+
+    while(abs(series(1, currentItteration)) > minError):
         currentItteration += 1
-    print()
-    print(currentItteration)
+    
+    print(currentItteration - 1)
 
 # Question 6: Determine the number of iterations necessary to solve f(x) = x^3 + 4x^2 - 10 = 0 with accuracy 10^-4
 #       a) Using the Bisection Method
-#       b)Using the Newton Raphson Method
+#       b) Using the Newton Raphson Method
 
-def custom_derivative(value):
-    return(3 * value * value) - (value ** 2) + 2
+#Part A(Bisection Method)
 
-def newton_raphson(initial_approximation: float, tolerance: float, sequence: str):
-    iteration_counter = 0
+def bisection(a: int, b: int, tolerance: float):
+    y = b - a
+    x = math.log(y,10)
+    z = math.log(tolerance, 10)
+    equation = (x - z) / (math.log(2,10))
+    print(math.ceil(equation))
 
-    # finds f
-    x = initial_approximation
-    f = eval(sequence)
 
-    # finds f'
-    f_prime = custom_derivative(initial_approximation)
+#def question_6():
+    #def f(x):
+       # return x**3 + 4(x**2) - 10
+    #def f_prime(x):
+        #return 3(x**2) + 8*x
+    #def my_bisection(f, a, b, tol):
+        #m = (a + b) / 2
+    #if np.abs(f(m)) < tol:
+       #return m
+    #elif np.sign(f(a)) == np.sign(f(m)):
+       #return my_bisection(f, m, b, tol)
+       #return my_bisection(f, m, b, tol) 
 
-    approximation: float = f / f_prime
-    while(abs(approximation) >= tolerance):
-        # finds f
-        x = initial_approximation
-        f = eval(sequence)
-        # finds f'
-        f_prime = custom_derivative(initial_approximation)
+#def my_newton(f, df, x0, tol):
 
-        # division operation
-        approximation =  f / f_prime
+   # if abs(f(x0)) < tol:
+        #return x0
+    #else: None
 
-        # subtraction operation
-        initial_approximation -= approximation
-        iteration_counter += 1
+#print(my_bisection(f, -4, 7, .0001))
+#print(my_newton(f, -4, 7, .0001))
+
 
 if __name__ == "__main__":
-    # Question 1:
-    print(double_precision())
+    # Question 1-4:
+    double_precision()
+    #Question 5
+    error()
     print("\n")
+    #Question 6
+    #Bisection Part (1)
+    a = -2
+    b = 5
+    tolerance: float = 10**-4
+    bisection(a, b, tolerance)
 
-    # Question 5:
-    # print("\n")
-    
-    # Question 6:
-    initial_approximation: float = -4
-    tolerance: float = 7
-    sequence: str = "(x**3) + 4*(x**2) - 10"
-    print(newton_raphson(initial_approximation, tolerance, sequence))
+
+    #print(newton_raphson(initial_approximation, tolerance, sequence))
+
 
